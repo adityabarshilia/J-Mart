4e0445b9d201365ad639d6b6e0774e12828070b9
Aditya Barshilia <37300568+adityabarshilia@users.noreply.github.com>
2022-08-28 22:46:36 +0530

    Revert "Revert "day-5""

--- scripts/review.js
+++ scripts/review.js
@@ -1,3 +1,37 @@
+let addressData = JSON.parse(localStorage.getItem('address'));
+let showAddress = document.getElementById('addressData');
+
+let form = document.querySelector('#address-form');
+let houseNo = document.getElementById('hno');
+let edit = document.getElementById('edit');
+
+adressdis()
+
+function adressdis() {
+    let addressData = JSON.parse(localStorage.getItem('address'));
+    if (addressData !== null) {
+        // showAddress.innerText = addressData.houseNo;
+        houseNo.innerText = addressData.houseNo;
+        document.getElementById("fno").innerText = addressData.floorNo;
+        document.getElementById("tno").innerText = addressData.towerNo;
+        document.getElementById("bname").innerText = addressData.buildingName;
+        document.getElementById("addr").innerText = addressData.address;
+        document.getElementById("lm").innerText = addressData.landmark;
+        document.getElementById("pin").innerText = addressData.pinCode;
+
+        showAddress.style.display = 'block';
+        edit.style.display = 'block'
+        form.style.display = "none";
+
+    }
+}
+
+function updateAddress() {
+    // showAddress.style.display = 'none';
+    // edit.style.display = 'none'
+    // form.style.display = "block";
+}
+
 
 setTimeout(() => {
     document.getElementById('loading_screen').style.display = "none";
@@ -6,13 +40,15 @@ setTimeout(() => {
 let count = JSON.parse(localStorage.getItem("count")) || 0;
 // count++;
 
+
 let data = JSON.parse(localStorage.getItem("jiomart")) || [];
 
 let productDetails = document.querySelector('.product-details');
 
 let availableAmt = document.getElementById('available-amt');
+
 let totalItems = document.getElementById('totalItems');
-totalItems.textContent = count;
+totalItems.textContent = data.length;
 
 // total price update 
 let mrp = document.getElementById('mrp');
@@ -28,7 +64,9 @@ function DisplayProducts(data) {
 
     data.forEach(function (el) {
 
-        total += Number(el.price)
+
+        total += (Number(el.price) * el.count);
+        let sum = total.toFixed(2);
 
         let productItems = document.createElement('div');
         productItems.setAttribute('class', 'product-itemsdetails');
@@ -49,7 +87,7 @@ function DisplayProducts(data) {
         productPrice.setAttribute('class', 'product-itemsPrice');
 
         let price1 = document.createElement('span');
-        price1.innerText = `₹${el.price}`
+        price1.innerText = `₹${el.price * el.count}`
         // let price2 = document.createElement('span');
         // price2.innerText = el.price;
         // let price3 = document.createElement('span');
@@ -62,7 +100,7 @@ function DisplayProducts(data) {
         let qty = document.createElement('div');
         qty.setAttribute('class', 'qyt');
         let p = document.createElement('p');
-        p.innerText = 'Qty';
+        p.innerText = `Qty ${el.count}`;
         let span = document.createElement('span');
         span.setAttribute('id', 'No_qyt');
 
@@ -87,11 +125,66 @@ function DisplayProducts(data) {
 
         productDetails.append(productItems, hr);
 
-        availableAmt.innerText = `₹${total}`
-        mrp.textContent = `₹${total}`
-        totalMRP.textContent = `₹${total}`
+        availableAmt.innerText = `₹${sum}`
+        mrp.textContent = `₹${sum}`
+        totalMRP.textContent = `₹${sum}`
 
 
     });
 }
 DisplayProducts(data);
+
+
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+
+    let dataOfAdress = {
+        houseNo: form.houseNo.value,
+        floorNo: form.floorNo.value,
+        towerNo: form.towerNo.value,
+        buildingName: form.buildingName.value,
+        address: address.value,
+        landmark: form.landmark.value,
+        pinCode: form.pinCode.value
+    }
+
+    // addressData.push(dataOfAdress);
+    localStorage.setItem('address', JSON.stringify(dataOfAdress));
+
+    addressData = JSON.parse(localStorage.getItem('address'));
+
+
+    console.log(addressData)
+    ////
+
+    showAddress.innerHTML = addressData.houseNo;
+    showAddress.style.display = 'block';
+
+    adressdis();
+
+
+
+});
+
+// coupon
+
+let totalAmout = JSON.parse(localStorage.getItem('subTotal'));
+
+// console.log(totalAmout * (0.3))
+
+let couponData = JSON.parse(localStorage.getItem("coupon"));
+let dc = couponData.disAmount;
+
+let ta = couponData.totalAmout;
+
+
+let disCoupon = document.getElementById('dis-Coupon');
+disCoupon.innerText = `₹${dc}`;
+
+let saveTotal = document.getElementById('total-save');
+saveTotal.innerText = `₹${dc}`;
+
+// let totalMRP = document.querySelector('#tota-price');
+totalMRP.innerText = `₹${ta}`;
