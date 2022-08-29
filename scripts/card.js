5a6ee62530868b47f78dd9bd7471a79442396f73
AdityaBarshilia <barshiliaaditya7@gmail.com>
2022-08-29 17:25:02 +0530

    updates

--- scripts/card.js
+++ scripts/card.js
@@ -1,302 +1,302 @@
 
-// let count = JSON.parse(localStorage.getItem("count")) || 0;
-// // count++;
+// // let count = JSON.parse(localStorage.getItem("count")) || 0;
+// // // count++;
 
-// let data = JSON.parse(localStorage.getItem("jiomart")) || [];
-// let count_display = document.getElementById('cart_counter');
-// function displayCount() {
-//     count_display.innerText = count;
-//     // console.log(count);
-// }
+// // let data = JSON.parse(localStorage.getItem("jiomart")) || [];
+// // let count_display = document.getElementById('cart_counter');
+// // function displayCount() {
+// //     count_display.innerText = count;
+// //     // console.log(count);
+// // }
 
 
-// showCart();
-// function showCart() {
-//     let data = JSON.parse(localStorage.getItem("jiomart")) || [];
-//     let total_items = document.getElementById('total_items');
-//     total_items.textContent = data.length;
-// }
+// // showCart();
+// // function showCart() {
+// //     let data = JSON.parse(localStorage.getItem("jiomart")) || [];
+// //     let total_items = document.getElementById('total_items');
+// //     total_items.textContent = data.length;
+// // }
 
-// let productDetails = document.querySelector('.product-details');
+// // let productDetails = document.querySelector('.product-details');
 
-// let availableAmt = document.getElementById('available-amt');
+// // let availableAmt = document.getElementById('available-amt');
 
-// let mrp = document.getElementById('mrp');
-// let totalMRP = document.querySelector('#tota-price');
+// // let mrp = document.getElementById('mrp');
+// // let totalMRP = document.querySelector('#tota-price');
 
 
 
 
 
-function DisplayProducts(data) {
+// function DisplayProducts(data) {
 
 
-// function DisplayProducts() {
-//     productDetails.innerHTML = null;
-//     let data = JSON.parse(localStorage.getItem("jiomart")) || [];
+// // function DisplayProducts() {
+// //     productDetails.innerHTML = null;
+// //     let data = JSON.parse(localStorage.getItem("jiomart")) || [];
 
 
-//     let total = 0;
+// //     let total = 0;
 
-//     data.forEach(function (el) {
+// //     data.forEach(function (el) {
 
-//         total += (Number(el.price) * el.count);
-//         let sum = total.toFixed(2);
+// //         total += (Number(el.price) * el.count);
+// //         let sum = total.toFixed(2);
 
 
 
-//         localStorage.setItem('subTotal', JSON.stringify(sum));
+// //         localStorage.setItem('subTotal', JSON.stringify(sum));
 
-//         let productItems = document.createElement('div');
-//         productItems.setAttribute('class', 'product-itemsdetails');
+// //         let productItems = document.createElement('div');
+// //         productItems.setAttribute('class', 'product-itemsdetails');
 
-//         let product_img = document.createElement('div');
-//         product_img.setAttribute('class', 'product-img');
+// //         let product_img = document.createElement('div');
+// //         product_img.setAttribute('class', 'product-img');
 
-//         let img = document.createElement('img');
-//         img.setAttribute('src', el.image);
+// //         let img = document.createElement('img');
+// //         img.setAttribute('src', el.image);
 
-//         let productName = document.createElement('div');
-//         productName.setAttribute('class', 'product-itemsName');
+// //         let productName = document.createElement('div');
+// //         productName.setAttribute('class', 'product-itemsName');
 
-//         let title = document.createElement('h4');
-//         title.innerText = el.title;
+// //         let title = document.createElement('h4');
+// //         title.innerText = el.title;
 
-//         let productPrice = document.createElement('div');
-//         productPrice.setAttribute('class', 'product-itemsPrice');
+// //         let productPrice = document.createElement('div');
+// //         productPrice.setAttribute('class', 'product-itemsPrice');
 
-//         let price1 = document.createElement('span');
-//         price1.innerText = `₹${el.price * el.count}`
-//         let price2 = document.createElement('span');
-//         price2.innerText = el.price;
-//         let price3 = document.createElement('span');
-//         price3.innerText = el.price;
+// //         let price1 = document.createElement('span');
+// //         price1.innerText = `₹${el.price * el.count}`
+// //         let price2 = document.createElement('span');
+// //         price2.innerText = el.price;
+// //         let price3 = document.createElement('span');
+// //         price3.innerText = el.price;
 
 
 
-//         let addRemoveBtn = document.createElement('div');
-//         addRemoveBtn.setAttribute('class', 'product-qty');
+// //         let addRemoveBtn = document.createElement('div');
+// //         addRemoveBtn.setAttribute('class', 'product-qty');
 
-//         let removeBtn = document.createElement('span');
-//         let img1 = document.createElement('img');
-//         img1.src = 'https://www.jiomart.com/msassets/images/icons/minus-bulecolor.svg';
+// //         let removeBtn = document.createElement('span');
+// //         let img1 = document.createElement('img');
+// //         img1.src = 'https://www.jiomart.com/msassets/images/icons/minus-bulecolor.svg';
 
 
-//         removeBtn.addEventListener('click', function () {
-//             removeCount(el.id);
-//         });
+// //         removeBtn.addEventListener('click', function () {
+// //             removeCount(el.id);
+// //         });
 
 
-//         let count = document.createElement('span');
-//         count.setAttribute("id", "showCount3");
-//         count.innerText = el.count;
-//         // document.getElementById("showCount3").innerText = ele.count;
+// //         let count = document.createElement('span');
+// //         count.setAttribute("id", "showCount3");
+// //         count.innerText = el.count;
+// //         // document.getElementById("showCount3").innerText = ele.count;
 
-//         let addBtn = document.createElement('span');
-//         addBtn.addEventListener('click', function () {
-//             addCount(el.id);
-//         });
+// //         let addBtn = document.createElement('span');
+// //         addBtn.addEventListener('click', function () {
+// //             addCount(el.id);
+// //         });
 
 
-//         let img2 = document.createElement('img');
-//         img2.src = "https://www.jiomart.com/msassets/images/icons/plus-bluecolor.svg"
-//         let div = document.createElement('div');
-//         let hr = document.createElement('hr');
+// //         let img2 = document.createElement('img');
+// //         img2.src = "https://www.jiomart.com/msassets/images/icons/plus-bluecolor.svg"
+// //         let div = document.createElement('div');
+// //         let hr = document.createElement('hr');
 
-//         productPrice.append(price1);
-//         productName.append(title, productPrice, addRemoveBtn);
-//         product_img.append(img);
+// //         productPrice.append(price1);
+// //         productName.append(title, productPrice, addRemoveBtn);
+// //         product_img.append(img);
 
 
-//         removeBtn.append(img1);
-//         addBtn.append(img2);
-//         addRemoveBtn.append(removeBtn, count, addBtn);
+// //         removeBtn.append(img1);
+// //         addBtn.append(img2);
+// //         addRemoveBtn.append(removeBtn, count, addBtn);
 
-//         div.append(hr)
+// //         div.append(hr)
 
 
-//         productItems.append(product_img, productName,);
+// //         productItems.append(product_img, productName,);
 
 
-//         productDetails.append(productItems, div);
+// //         productDetails.append(productItems, div);
 
-        total += Number(el.price);
+//         total += Number(el.price);
 
 
-//         availableAmt.textContent = `₹${sum}`
-//         mrp.textContent = `₹${sum}`
-//         totalMRP.textContent = `₹${sum}`
+// //         availableAmt.textContent = `₹${sum}`
+// //         mrp.textContent = `₹${sum}`
+// //         totalMRP.textContent = `₹${sum}`
 
-//         // total_items.innerText = count;
+// //         // total_items.innerText = count;
 
-//     });
-// }
-
-
-
-// DisplayProducts();
-
-
-// function addCount(id) {
-//     let data = JSON.parse(localStorage.getItem("jiomart")) || [];
-//     let index = null;
-
-//     for (let i = 0; i < data.length; i++) {
-//         if (data[i].id == id) {
-//             ``
-//             index = i;
-//         }
-//     }
-
-//     if (index !== null && data[index].count < 5) {
-//         data[index].count++;
-//         count++;
-//         localStorage.setItem("count", JSON.stringify(count));
-//         localStorage.setItem('jiomart', JSON.stringify(data));
-//         DisplayProducts();
-//         showCart()
-//         displayCount()
-//     } else {
-//         alert("Max");
-//     }
-
-
-
-//     // console.log(1)
-// }
-
-
-
-
-// function removeCount(id) {
-//     let data = JSON.parse(localStorage.getItem("jiomart")) || [];
-//     let index = null;
-
-
-//     for (let i = 0; i < data.length; i++) {
-//         if (data[i].id == id) {
-//             data[i].count--;
-//             count--;
-//             localStorage.setItem("count", JSON.stringify(count));
-//             index = i;
-//             break;
-//         }
-//     }
-
-//     if (index !== null && data[index].count <= 0) {
-//         data.splice(index, 1);
-//     }
-
-//     localStorage.setItem('jiomart', JSON.stringify(data));
-//     DisplayProducts();
-//     showCart()
-//     displayCount();
-
-// }
-
-// // if (data.length == 0) {
-
+// //     });
 // // }
 
 
 
-// coupon code //
-
-let totalAmout = JSON.parse(localStorage.getItem('subTotal'));
-
-// console.log(totalAmout * (0.3))
-
-let couponData = JSON.parse(localStorage.getItem("coupon"));
-let dc = couponData.disAmount;
-
-let ta = couponData.totalAmout;
+// // DisplayProducts();
 
 
-let disCoupon = document.getElementById('dis-Coupon');
-disCoupon.innerText = `₹${dc}`;
+// // function addCount(id) {
+// //     let data = JSON.parse(localStorage.getItem("jiomart")) || [];
+// //     let index = null;
 
-let saveTotal = document.getElementById('total-save');
-saveTotal.innerText = `₹${dc}`;
+// //     for (let i = 0; i < data.length; i++) {
+// //         if (data[i].id == id) {
+// //             ``
+// //             index = i;
+// //         }
+// //     }
 
-let totalMRP = document.querySelector('#tota-price');
-totalMRP.innerText = `₹${ta}`;
-
-
-function checkCoupon() {
-
-    let code = document.getElementById("cCode");
-    let disAmount = document.getElementById('dis-Coupon');
-    let totalPrice = document.getElementById('tota-price');
-
-
-    if (code.value === "masai30") {
-
-        let dis = Math.floor(totalAmout * (0.3));
-        let tp = Math.floor((0.7) * totalAmout);
-
-        disAmount.innerHTML = `₹${dis}`
-        totalPrice.textContent = `₹${tp}`;
-
-        let coupon = {
-            disAmount: dis,
-            totalAmout: tp
-        }
-
-        localStorage.setItem('coupon', JSON.stringify(coupon));
-
-        alert('Coupon Applied Successfully');
-
-    } else {
-        alert("This Code is Not Valid");
-    }
-
-}
+// //     if (index !== null && data[index].count < 5) {
+// //         data[index].count++;
+// //         count++;
+// //         localStorage.setItem("count", JSON.stringify(count));
+// //         localStorage.setItem('jiomart', JSON.stringify(data));
+// //         DisplayProducts();
+// //         showCart()
+// //         displayCount()
+// //     } else {
+// //         alert("Max");
+// //     }
 
 
 
-function placeOrder() {
-    // let disAmount = document.getElementById("dis-Coupon");
-    // let totalPrice = document.getElementById("totalPrice");
-
-
-    window.location.href = "review.html"
-}
-
-
-document.getElementById("couponApply").addEventListener("click", function(){
-    console.log("count");
-    checkCoupon();
-})
+// //     // console.log(1)
+// // }
 
 
 
-function checkCoupon() {
 
-    let code = document.getElementById("cCode");
-    let disAmount = document.getElementById('dis-Coupon');
-    let totalPrice = document.getElementById('tota-price');
+// // function removeCount(id) {
+// //     let data = JSON.parse(localStorage.getItem("jiomart")) || [];
+// //     let index = null;
+
+
+// //     for (let i = 0; i < data.length; i++) {
+// //         if (data[i].id == id) {
+// //             data[i].count--;
+// //             count--;
+// //             localStorage.setItem("count", JSON.stringify(count));
+// //             index = i;
+// //             break;
+// //         }
+// //     }
+
+// //     if (index !== null && data[index].count <= 0) {
+// //         data.splice(index, 1);
+// //     }
+
+// //     localStorage.setItem('jiomart', JSON.stringify(data));
+// //     DisplayProducts();
+// //     showCart()
+// //     displayCount();
+
+// // }
+
+// // // if (data.length == 0) {
+
+// // // }
+
+
+
+// // coupon code //
+
+// let totalAmout = JSON.parse(localStorage.getItem('subTotal'));
+
+// // console.log(totalAmout * (0.3))
+
+// let couponData = JSON.parse(localStorage.getItem("coupon"));
+// let dc = couponData.disAmount;
+
+// let ta = couponData.totalAmout;
+
+
+// let disCoupon = document.getElementById('dis-Coupon');
+// disCoupon.innerText = `₹${dc}`;
+
+// let saveTotal = document.getElementById('total-save');
+// saveTotal.innerText = `₹${dc}`;
+
+// let totalMRP = document.querySelector('#tota-price');
+// totalMRP.innerText = `₹${ta}`;
+
+
+// function checkCoupon() {
+
+//     let code = document.getElementById("cCode");
+//     let disAmount = document.getElementById('dis-Coupon');
+//     let totalPrice = document.getElementById('tota-price');
+
+
+//     if (code.value === "masai30") {
+
+//         let dis = Math.floor(totalAmout * (0.3));
+//         let tp = Math.floor((0.7) * totalAmout);
+
+//         disAmount.innerHTML = `₹${dis}`
+//         totalPrice.textContent = `₹${tp}`;
+
+//         let coupon = {
+//             disAmount: dis,
+//             totalAmout: tp
+//         }
+
+//         localStorage.setItem('coupon', JSON.stringify(coupon));
+
+//         alert('Coupon Applied Successfully');
+
+//     } else {
+//         alert("This Code is Not Valid");
+//     }
+
+// }
+
+
+
+// function placeOrder() {
+//     // let disAmount = document.getElementById("dis-Coupon");
+//     // let totalPrice = document.getElementById("totalPrice");
+
+
+//     window.location.href = "review.html"
+// }
+
+
+// document.getElementById("couponApply").addEventListener("click", function(){
+//     console.log("count");
+//     checkCoupon();
+// })
+
+
+
+// function checkCoupon() {
+
+//     let code = document.getElementById("cCode");
+//     let disAmount = document.getElementById('dis-Coupon');
+//     let totalPrice = document.getElementById('tota-price');
     
 
-    if (code.value === "masai30") {
+//     if (code.value === "masai30") {
 
-        let dis = Math.floor(totalAmout * (0.3));
-        let tp = Math.floor((0.7) * totalAmout);
+//         let dis = Math.floor(totalAmout * (0.3));
+//         let tp = Math.floor((0.7) * totalAmout);
 
-        disAmount.innerHTML = `₹${dis}`
-        totalPrice.textContent = `₹${tp}`;
+//         disAmount.innerHTML = `₹${dis}`
+//         totalPrice.textContent = `₹${tp}`;
 
-        let coupon = {
-            disAmount: dis,
-            totalAmout: tp
-        }
+//         let coupon = {
+//             disAmount: dis,
+//             totalAmout: tp
+//         }
 
-        localStorage.setItem('coupon', JSON.stringify(coupon));
+//         localStorage.setItem('coupon', JSON.stringify(coupon));
 
-        alert('Coupon Applied Successfully');
+//         alert('Coupon Applied Successfully');
 
-    } else {
-        alert("This Code is Not Valid");
-    }
-    alert("This Code is Not Valid");
-}
-}
+//     } else {
+//         alert("This Code is Not Valid");
+//     }
+//     alert("This Code is Not Valid");
+// }
+// }
--- scripts/review.js
+++ scripts/review.js
@@ -1,190 +1,193 @@
-let addressData = JSON.parse(localStorage.getItem('address'));
-let showAddress = document.getElementById('addressData');
+let addressData = JSON.parse(localStorage.getItem("address"));
+let signUpData = JSON.parse(localStorage.getItem('signupData'));
 
-let form = document.querySelector('#address-form');
-let houseNo = document.getElementById('hno');
-let edit = document.getElementById('edit');
+let showAddress = document.getElementById("addressData");
 
-adressdis()
+let form = document.querySelector("#address-form");
+// let houseNo = document.getElementById("hno");
+let edit = document.getElementById("edit");
+let editbtn = document.getElementById("editbtn");
+let username = document.getElementById("user");
+
+username.innerText = `${signUpData.firstname} ${signUpData.lastname}`;
+
+editbtn.addEventListener('click', toggleForm);
+
+function toggleForm(){
+    form.style.display = 'grid';
+    showAddress.style.display = "none";
+    edit.style.display = "none";
+}
+
+adressdis();
 
 function adressdis() {
-    let addressData = JSON.parse(localStorage.getItem('address'));
-    if (addressData !== null) {
-        // showAddress.innerText = addressData.houseNo;
-        houseNo.innerText = addressData.houseNo;
-        document.getElementById("fno").innerText = addressData.floorNo;
-        document.getElementById("tno").innerText = addressData.towerNo;
-        document.getElementById("bname").innerText = addressData.buildingName;
-        document.getElementById("addr").innerText = addressData.address;
-        document.getElementById("lm").innerText = addressData.landmark;
-        document.getElementById("pin").innerText = addressData.pinCode;
+  addressData = JSON.parse(localStorage.getItem("address"));
+  if (addressData !== null) {
+    
+    showAddress.style.display = "block";
+   
+    document.getElementById("fno").innerText = addressData.houseNo;
+    document.getElementById("fno").innerText = addressData.floorNo;
+    document.getElementById("tno").innerText = addressData.towerNo;
+    document.getElementById("bname").innerText = addressData.buildingName;
+    document.getElementById("addr").innerText = addressData.address;
+    document.getElementById("lm").innerText = addressData.landmark;
+    document.getElementById("pin").innerText = addressData.pinCode;
 
-        showAddress.style.display = 'block';
-        edit.style.display = 'block'
-        form.style.display = "none";
-
-    }
+   
+    edit.style.display = "block";
+    form.style.display = "none";
+  }
 }
 
 function updateAddress() {
-    // showAddress.style.display = 'none';
-    // edit.style.display = 'none'
-    // form.style.display = "block";
+  // showAddress.style.display = 'none';
+  // edit.style.display = 'none'
+  // form.style.display = "block";
 }
 
-
 setTimeout(() => {
-    document.getElementById('loading_screen').style.display = "none";
+  document.getElementById("loading_screen").style.display = "none";
 }, 1500);
 
 let count = JSON.parse(localStorage.getItem("count")) || 0;
 // count++;
 
-
 let data = JSON.parse(localStorage.getItem("jiomart")) || [];
 
-let productDetails = document.querySelector('.product-details');
+let productDetails = document.querySelector(".product-details");
 
-let availableAmt = document.getElementById('available-amt');
+let availableAmt = document.getElementById("available-amt");
 
-let totalItems = document.getElementById('totalItems');
+let totalItems = document.getElementById("totalItems");
 totalItems.textContent = data.length;
 
-// total price update 
-let mrp = document.getElementById('mrp');
-let totalMRP = document.getElementById('tota-price');
-
+// total price update
+let mrp = document.getElementById("mrp");
+let totalMRP = document.getElementById("tota-price");
 
 function DisplayProducts(data) {
+  // productDetails.innerHTML = null;
 
+  let total = 0;
 
-    // productDetails.innerHTML = null;
+  data.forEach(function (el) {
+    total += Number(el.price) * el.count;
+    let sum = total.toFixed(2);
 
-    let total = 0;
+    let productItems = document.createElement("div");
+    productItems.setAttribute("class", "product-itemsdetails");
 
-    data.forEach(function (el) {
+    let product_img = document.createElement("div");
+    product_img.setAttribute("class", "product-img");
 
+    let img = document.createElement("img");
+    img.setAttribute("src", el.image);
 
-        total += (Number(el.price) * el.count);
-        let sum = total.toFixed(2);
+    let productName = document.createElement("div");
+    productName.setAttribute("class", "product-itemsName");
 
-        let productItems = document.createElement('div');
-        productItems.setAttribute('class', 'product-itemsdetails');
+    let title = document.createElement("h4");
+    title.innerText = el.title;
 
-        let product_img = document.createElement('div');
-        product_img.setAttribute('class', 'product-img');
+    let productPrice = document.createElement("div");
+    productPrice.setAttribute("class", "product-itemsPrice");
 
-        let img = document.createElement('img');
-        img.setAttribute('src', el.image);
+    let price1 = document.createElement("span");
+    price1.innerText = `₹${el.price * el.count}`;
+    // let price2 = document.createElement('span');
+    // price2.innerText = el.price;
+    // let price3 = document.createElement('span');
+    // price3.innerText = el.price;
 
-        let productName = document.createElement('div');
-        productName.setAttribute('class', 'product-itemsName');
+    let qtyBox = document.createElement("div");
+    qtyBox.setAttribute("id", "qtyBox");
 
-        let title = document.createElement('h4');
-        title.innerText = el.title;
+    let qty = document.createElement("div");
+    qty.setAttribute("class", "qyt");
+    let p = document.createElement("p");
+    p.innerText = `Qty ${el.count}`;
+    let span = document.createElement("span");
+    span.setAttribute("id", "No_qyt");
 
-        let productPrice = document.createElement('div');
-        productPrice.setAttribute('class', 'product-itemsPrice');
+    let div = document.createElement("div");
+    let para = document.createElement("p");
+    para.innerText = "Delivery by tomorrow";
 
-        let price1 = document.createElement('span');
-        price1.innerText = `₹${el.price * el.count}`
-        // let price2 = document.createElement('span');
-        // price2.innerText = el.price;
-        // let price3 = document.createElement('span');
-        // price3.innerText = el.price;
+    productPrice.append(price1);
+    productName.append(title, productPrice, qtyBox);
+    product_img.append(img);
 
+    qty.append(p, span);
+    div.append(para);
 
-        let qtyBox = document.createElement('div');
-        qtyBox.setAttribute('id', 'qtyBox');
+    qtyBox.append(qty, div);
 
-        let qty = document.createElement('div');
-        qty.setAttribute('class', 'qyt');
-        let p = document.createElement('p');
-        p.innerText = `Qty ${el.count}`;
-        let span = document.createElement('span');
-        span.setAttribute('id', 'No_qyt');
+    let hr = document.createElement("hr");
 
-        let div = document.createElement('div');
-        let para = document.createElement('p');
-        para.innerText = 'Delivery by tomorrow';
+    productItems.append(product_img, productName);
 
+    productDetails.append(productItems, hr);
 
-        productPrice.append(price1);
-        productName.append(title, productPrice, qtyBox);
-        product_img.append(img);
-
-        qty.append(p, span);
-        div.append(para);
-
-        qtyBox.append(qty, div);
-
-        let hr = document.createElement('hr')
-
-
-        productItems.append(product_img, productName,);
-
-        productDetails.append(productItems, hr);
-
-        availableAmt.innerText = `₹${sum}`
-        mrp.textContent = `₹${sum}`
-        totalMRP.textContent = `₹${sum}`
-
-
-    });
+    availableAmt.innerText = `₹${sum}`;
+    mrp.textContent = `₹${sum}`;
+    totalMRP.textContent = `₹${sum}`;
+  });
 }
 DisplayProducts(data);
 
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
+  let dataOfAdress = {
+    houseNo: form.houseNo.value,
+    floorNo: form.floorNo.value,
+    towerNo: form.towerNo.value,
+    buildingName: form.buildingName.value,
+    address: address.value,
+    landmark: form.landmark.value,
+    pinCode: form.pinCode.value,
+  };
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
+  // addressData.push(dataOfAdress);
+  localStorage.setItem("address", JSON.stringify(dataOfAdress));
 
+  addressData = JSON.parse(localStorage.getItem("address"));
 
-    let dataOfAdress = {
-        houseNo: form.houseNo.value,
-        floorNo: form.floorNo.value,
-        towerNo: form.towerNo.value,
-        buildingName: form.buildingName.value,
-        address: address.value,
-        landmark: form.landmark.value,
-        pinCode: form.pinCode.value
-    }
+  console.log(addressData);
+  ////
 
-    // addressData.push(dataOfAdress);
-    localStorage.setItem('address', JSON.stringify(dataOfAdress));
+//   showAddress.innerHTML = addressData.houseNo;
+  showAddress.style.display = "block";
 
-    addressData = JSON.parse(localStorage.getItem('address'));
-
-
-    console.log(addressData)
-    ////
-
-    showAddress.innerHTML = addressData.houseNo;
-    showAddress.style.display = 'block';
-
-    adressdis();
-
-
-
+  adressdis();
 });
 
 // coupon
 
-let totalAmout = JSON.parse(localStorage.getItem('subTotal'));
+let totalAmout = JSON.parse(localStorage.getItem("subTotal"));
 
 // console.log(totalAmout * (0.3))
 
 let couponData = JSON.parse(localStorage.getItem("coupon"));
-let dc = couponData.disAmount;
+let disCoupon = document.getElementById("dis-Coupon");
+let saveTotal = document.getElementById("total-save");
+ let dc, ta;
 
-let ta = couponData.totalAmout;
+if (couponData !== null) {
+  dc = couponData.disAmount;
 
+   ta = couponData.totalAmout;
 
-let disCoupon = document.getElementById('dis-Coupon');
-disCoupon.innerText = `₹${dc}`;
+//   console.log(disCoupon)
+  disCoupon.innerText = `₹${dc}`;
 
-let saveTotal = document.getElementById('total-save');
-saveTotal.innerText = `₹${dc}`;
+  saveTotal.innerText = `₹${dc}`;saveTotal.innerText = `₹${dc}`;
 
-// let totalMRP = document.querySelector('#tota-price');
-totalMRP.innerText = `₹${ta}`;
+  // let totalMRP = document.querySelector('#tota-price');
+  totalMRP.innerText = `₹${ta}`;
+}else{
+    disCoupon.innerText = `₹0`;
+    saveTotal.innerText = `₹0`;
+}
